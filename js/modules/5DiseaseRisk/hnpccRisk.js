d54536b574297f198a640d68855ec1c44da0d749
Hiroyuki Ono <Ono.Hiroyuki.we@mesw.co.jp>
2022-09-09 18:21:20 +0900

    コメントの追記

--- js/modules/5DiseaseRisk/hnpccRisk.js
+++ js/modules/5DiseaseRisk/hnpccRisk.js
@@ -1,9 +1,9 @@
 /**
  * 2022/06/06
  * 
- * HNPCC疾患リスク
+ * リンチ症候群（遺伝性非ポリポーシス大腸がん）HNPCC疾患リスク
  */
-class HnpccRisk extends FiveDiseaseRiskBase {
+export class HnpccRisk extends FiveDiseaseRiskBase {
     appliesToAmsterdamStanderd2() {
 
     }
@@ -16,47 +16,86 @@ class HnpccRisk extends FiveDiseaseRiskBase {
 
     }
 
+    /**
+     * C1：少なくとも3人の血縁者がHNPCC（リンチ症候群）関連腫瘍（大腸癌，子宮内膜癌，腎盂・尿管癌，小腸癌）に罹患しており，以下のすべてを満たしている。
+     *  1．1人の罹患者はその他の2人に対して第1度近親者である。
+     *  2．少なくとも連続する2世代で罹患している。
+     *  3．少なくとも1人の癌は50歳未満で診断されている。
+     *  4．腫瘍は病理学的に癌であることが確認されている。
+     *  5．FAPが除外されている。
+     */
     _areOver3PersonOnsetOfHnpcc() {
 
     }
 
-    _isOnePersonBelongToFirstDegreeRelatives() {
+    /**
+     * C1-1：1．1人の罹患者はその他の2人に対して第1度近親者である。
+     */
+     _isOnePersonBelongToFirstDegreeRelatives() {
 
     }
 
-    _isOnsetFor2ConsecutiveGenerations() {
+    /**
+     * C1-2：2．少なくとも連続する2世代で罹患している。
+     */
+     _isOnsetFor2ConsecutiveGenerations() {
 
     }
 
-    _isOnePersonOnsetOfHnpccAtlessThan50YearsOlds() {
+    /**
+     * C1-3：3．少なくとも1人の癌は50歳未満で診断されている。
+     */
+     _isOnePersonOnsetOfHnpccAtlessThan50YearsOlds() {
 
     }
 
-    _isPolypDetectedAsCanser() {
+    /**
+     * C1-4：4．腫瘍は病理学的に癌であることが確認されている。
+     */
+     _isPolypDetectedAsCanser() {
 
     }
 
-    _isFapExcluded() {
+    /**
+     * C1-5：5．FAPが除外されている。
+     */
+     _isFapExcluded() {
 
     }
 
+    /**
+     * C2：本人が大腸がん患者かつ、50歳未満で診断された大腸癌
+     */
     _isOnsetOfColorectalCancerAtLessThanOrEqualTo45YearsOld() {
 
     }
 
-    _isOnsetOfColorectalCancerAndHnpcc() {
+    /**
+     * C3：本人が大腸がん患者かつ、年齢に関わりなく，同時性あるいは異時性大腸癌あるいはその他のリンチ症候群関連腫瘍＊がある。
+     * ＊：大腸癌，子宮内膜癌，胃癌，卵巣癌，膵癌，胆道癌，小腸癌，腎盂・尿管癌，脳腫瘍（通常はターコット症候群にみられるglioblastoma），ムア・トレ症候群の皮脂腺腫や角化棘細胞腫
+     */
+     _isOnsetOfColorectalCancerAndHnpcc() {
 
     }
 
-    _isOnsetOfColorectalCancerAndOnePersonOnsetOfHnpccAtLessThan50YearsOld() {
+    /**
+     * C5：本人が大腸がん患者かつ、第1度近親者が1人以上リンチ症候群関連腫瘍に罹患しており，そのうち一つは50歳未満で診断された大腸癌
+     */
+     _isOnsetOfColorectalCancerAndOnePersonOnsetOfHnpccAtLessThan50YearsOld() {
 
     }
 
-    _isOnsetOfColorectalCancer() {
+    /**
+     * C6：本人が大腸がん患者かつ、年齢に関わりなく，第1度あるいは第2度近親者の2人以上がリンチ症候群関連腫瘍と診断されている患者の大腸癌
+     */
+     _isOnsetOfColorectalCancer() {
 
     }
 
-    _isOnsetOfColorectalOrEndometrialCancer() {
+    /**
+     * C7：全て（あるいは 70 歳以下）の大腸癌、子宮内膜がん
+     */
+     _isOnsetOfColorectalOrEndometrialCancer() {
 
     }
 }
