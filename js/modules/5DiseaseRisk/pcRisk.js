e56ddbb9a5a60c56f6c4bd06e0842cbe55e9491b
Hiroyuki Ono <Ono.Hiroyuki.we@mesw.co.jp>
2022-09-09 19:16:50 +0900

    前立腺がん疾患リスク計算のテストクラスの空メソッド追加

--- js/modules/5DiseaseRisk/pcRisk.js
+++ js/modules/5DiseaseRisk/pcRisk.js
@@ -9,7 +9,7 @@ export class PcRisk extends FiveDiseaseRiskBase {
     }
 
     /**
-     * CQ5.「人間ドック等の受益者負担による検診においては，前立腺癌の家族歴を有する場合には40歳代からの受診を推奨し…」
+     * P1：CQ5.「人間ドック等の受益者負担による検診においては，前立腺癌の家族歴を有する場合には40歳代からの受診を推奨し…」
      * ◆CQ4. 日本での疫学調査により「第1度近親者に1人の前立腺癌患者がいる場合の罹患リスクは5.6倍（95％CI：1.5〜20.5）と有意差を認めた」
      */
     _hasPersonOnsetOfProstateCanserWithinFirstDegreeRelaives() {
