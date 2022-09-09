d2574dea72761b8edcc2058d9bf5be0e4abd3967
Hiroyuki Ono <Ono.Hiroyuki.we@mesw.co.jp>
2022-09-09 18:28:23 +0900

    コメント追記

--- js/modules/5DiseaseRisk/aaaRisk.js
+++ js/modules/5DiseaseRisk/aaaRisk.js
@@ -1,13 +1,16 @@
 /**
  * 2022/-6/06
  * 
- * AAA疾患リスク
+ * 腹部大動脈瘤AAA疾患リスク
  */
 class AaaRisk extends FiveDiseaseRiskBase {
     recommendScreeningByAbdominalEchography() {
 
     }
 
+    /**
+     * A1~A3：65歳以上の男性や65歳以上の喫煙女性，第一度近親者に家族歴を有する者
+     */
     _isSmokingOrHavingPersonWhoHasAaaWithinFirstDegreeRelatives() {
         
     }
