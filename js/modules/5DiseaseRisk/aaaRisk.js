39cedba34dd768870a8276bed3e3dbb7e09015cf
Hiroyuki Ono <Ono.Hiroyuki.we@mesw.co.jp>
2022-09-20 19:19:10 +0900

    腹部大動脈瘤のリスク判定ロジックを実装

--- js/modules/5DiseaseRisk/aaaRisk.js
+++ js/modules/5DiseaseRisk/aaaRisk.js
@@ -3,15 +3,39 @@
  * 
  * 腹部大動脈瘤AAA疾患リスク
  */
-class AaaRisk extends FiveDiseaseRiskBase {
-    recommendScreeningByAbdominalEchography() {
+ import { FiveDiseaseRiskCommons } from '../5DiseaseRisk/5DiseaseRiskCommons';
+ import { FiveDiseaseRiskBase } from '../5DiseaseRisk/5DiseaseRiskBase';
+ import { RaceUtil, RelativeUtil, CodeUtil, NoteUtil, ValueUtil } from '../xmlTagUtil';
 
+export class AaaRisk extends FiveDiseaseRiskBase {
+    recommendScreeningByAbdominalEchography(pi) {
+        return this._isSmokingOrHavingPersonWhoHasAaaWithinFirstDegreeRelatives(pi);
     }
 
     /**
      * A1~A3：65歳以上の男性や65歳以上の喫煙女性，第一度近親者に家族歴を有する者
      */
-    _isSmokingOrHavingPersonWhoHasAaaWithinFirstDegreeRelatives() {
-        
+    _isSmokingOrHavingPersonWhoHasAaaWithinFirstDegreeRelatives(pi) {
+        // 65歳以上の男性
+        if(FiveDiseaseRiskCommons.isAgeGreaterThanOrEqualTo(65, pi)
+            && FiveDiseaseRiskCommons.isGenderMatch("MALE", pi)){
+            return true;
+        }
+
+        // 65歳以上で現在喫煙中の女性
+        if(FiveDiseaseRiskCommons.isAgeGreaterThanOrEqualTo(65, pi)
+            && FiveDiseaseRiskCommons.isGenderMatch("FEMALE", pi)
+            && pi.smoker=="5"){
+            return true;
+        }
+
+        // 第一度近親者に腹部大動脈瘤の病歴がある
+        var relatives = RelativeUtil.getFirstDegreeRelatives();
+        var numberOfPerson = FiveDiseaseRiskCommons.countDiseasePersonInRelatives(relatives, 'SNOMED_CT-75878002', pi);
+
+        if(numberOfPerson >= 1){
+            return true;
+        }
+        return false;
     }
 }
