4ec76872a0b6cbfb4c679c6b2109354ee5f335b7
Hiroyuki Ono <Ono.Hiroyuki.we@mesw.co.jp>
2022-09-20 17:47:40 +0900

    前立腺がんリスク判定ロジックを実装

--- js/modules/5DiseaseRisk/pcRisk.js
+++ js/modules/5DiseaseRisk/pcRisk.js
@@ -3,16 +3,39 @@
  * 
  * 前立腺がん疾患リスク
  */
-export class PcRisk extends FiveDiseaseRiskBase {
-    recomendInspectProstateCancer() {
 
+import { FiveDiseaseRiskCommons } from '../5DiseaseRisk/5DiseaseRiskCommons';
+import { FiveDiseaseRiskBase } from '../5DiseaseRisk/5DiseaseRiskBase';
+import { RaceUtil, RelativeUtil, CodeUtil, NoteUtil, ValueUtil } from '../xmlTagUtil';
+
+export class PcRisk extends FiveDiseaseRiskBase {
+    recomendInspectProstateCancer(pi) {
+        // 本人が女性かどうか判断する。
+        if(FiveDiseaseRiskCommons.isGenderMatch("FEMALE", pi)){
+            return false;
+        }
+
+        // 本人が40歳以上
+        if(FiveDiseaseRiskCommons.isAgeGreaterThanOrEqualTo(40, pi)){
+            return true;
+        }
+
+        // 第一度近親者に前立腺がん
+        return this._hasPersonOnsetOfProstateCanserWithinFirstDegreeRelaives(pi);
     }
 
     /**
      * P1：CQ5.「人間ドック等の受益者負担による検診においては，前立腺癌の家族歴を有する場合には40歳代からの受診を推奨し…」
      * ◆CQ4. 日本での疫学調査により「第1度近親者に1人の前立腺癌患者がいる場合の罹患リスクは5.6倍（95％CI：1.5〜20.5）と有意差を認めた」
      */
-    _hasPersonOnsetOfProstateCanserWithinFirstDegreeRelaives() {
+    _hasPersonOnsetOfProstateCanserWithinFirstDegreeRelaives(pi) {
+        // 第一
+        var relative = RelativeUtil.getFirstDegreeRelatives();
+        var numberOfPerson = FiveDiseaseRiskCommons.countDiseasePersonInRelatives(relative, 'SNOMED_CT-399068003', pi);
 
+        if(numberOfPerson >= 1){
+            return true;
+        }
+        return false;
     }
 }
