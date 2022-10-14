fc4cbee1c83734219233ae5d6368a082128d1463
Hiroyuki Ono <Ono.Hiroyuki.we@mesw.co.jp>
2022-10-14 17:42:25 +0900

    リンチ症候群のメソッドを追記

--- js/modules/5DiseaseRisk/hnpccRisk.js
+++ js/modules/5DiseaseRisk/hnpccRisk.js
@@ -46,35 +46,35 @@ export class HnpccRisk extends FiveDiseaseRiskBase {
     /**
      * C1-1：1．1人の罹患者はその他の2人に対して第1度近親者である。
      */
-     _isOnePersonBelongToFirstDegreeRelatives(piArray) {
+    _isOnePersonBelongToFirstDegreeRelatives(piArray) {
 
     }
 
     /**
      * C1-2：2．少なくとも連続する2世代で罹患している。
      */
-     _isOnsetFor2ConsecutiveGenerations(piArray) {
+    _isOnsetFor2ConsecutiveGenerations(piArray) {
 
     }
 
     /**
      * C1-3：3．少なくとも1人の癌は50歳未満で診断されている。
      */
-     _isOnePersonOnsetOfHnpccAtlessThan50YearsOlds(piArray) {
+    _isOnePersonOnsetOfHnpccAtlessThan50YearsOlds(piArray) {
 
     }
 
     /**
      * C1-4：4．腫瘍は病理学的に癌であることが確認されている。※実装無のため常にtrueを返す。
      */
-     _isPolypDetectedAsCanser(piArray) {
+    _isPolypDetectedAsCanser(piArray) {
         return true;
     }
 
     /**
      * C1-5：5．FAPが除外されている。※実装無のため常にtrueを返す。
      */
-     _isFapExcluded(piArray) {
+    _isFapExcluded(piArray) {
         return true;
     }
 
@@ -89,28 +89,28 @@ export class HnpccRisk extends FiveDiseaseRiskBase {
      * C3：本人が大腸がん患者かつ、年齢に関わりなく，同時性あるいは異時性大腸癌あるいはその他のリンチ症候群関連腫瘍＊がある。
      * ＊：大腸癌，子宮内膜癌，胃癌，卵巣癌，膵癌，胆道癌，小腸癌，腎盂・尿管癌，脳腫瘍（通常はターコット症候群にみられるglioblastoma），ムア・トレ症候群の皮脂腺腫や角化棘細胞腫
      */
-     _isOnsetOfColorectalCancerAndHnpcc() {
+    _isOnsetOfColorectalCancerAndHnpcc() {
 
     }
 
     /**
      * C5：本人が大腸がん患者かつ、第1度近親者が1人以上リンチ症候群関連腫瘍に罹患しており，そのうち一つは50歳未満で診断された大腸癌
      */
-     _isOnsetOfColorectalCancerAndOnePersonOnsetOfHnpccAtLessThan50YearsOld() {
+    _isOnsetOfColorectalCancerAndOnePersonOnsetOfHnpccAtLessThan50YearsOld() {
 
     }
 
     /**
      * C6：本人が大腸がん患者かつ、年齢に関わりなく，第1度あるいは第2度近親者の2人以上がリンチ症候群関連腫瘍と診断されている患者の大腸癌
      */
-     _isOnsetOfColorectalCancer() {
+    _isOnsetOfColorectalCancer() {
 
     }
 
     /**
      * C7：全て（あるいは 70 歳以下）の大腸癌、子宮内膜がん
      */
-     _isOnsetOfColorectalOrEndometrialCancer() {
-
+    _isOnsetOfColorectalOrEndometrialCancer(pi) {
+        return FiveDiseaseRiskCommons.isDiesaseMatchOr(['SNOMED_CT-1000001','SNOMED_CT-188192002'],pi);
     }
 }
